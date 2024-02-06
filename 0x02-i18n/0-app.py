aa8a32c70d386a3cb37d67c6771b4dfd6d1a4e17
RedaRahmani <mohamedreda.rahmani20@ump.ac.ma>
2024-02-06 12:35:56 +0100

    Fix Task01

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -8,4 +8,4 @@ def home():
     return render_template('0-index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True , host= '0.0.0.0' , port=4000)
+    app.run(debug=True , host= '0.0.0.0' , port=5000)
